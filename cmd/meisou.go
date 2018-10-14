c7ba7f376b4a518cc68aeb4c685954f46e2f3ff5
him0 <hiroki-nakashima@c-fo.com>
2018-10-15 02:38:07 +0900

    :sparkles: meisou コマンド調整

--- cmd/meisou.go
+++ cmd/meisou.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+
 	"github.com/cheggaaa/pb"
 	"github.com/urfave/cli"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/neurosky"
-	"log"
-	"os"
-	"strconv"
 )
 
 func main() {
@@ -29,11 +31,18 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
+		command := c.Args().Get(0)
 		fmt.Println("目標瞑想力:" + strconv.Itoa(int(border)) + "%")
+		if command != "" {
+			fmt.Println("実行コマンド:" + command)
+		}
 		meditation := 0.0
 		basePart := 0.9
 		currentPart := 1.0 - basePart
-		bar := pb.StartNew(int(border))
+
+		bar := pb.New(int(border))
+		bar.ShowTimeLeft = false
+		bar.ShowSpeed = false
 
 		adaptor := neurosky.NewAdaptor("/dev/tty.MindWaveMobile-SerialPo")
 		neuro := neurosky.NewDriver(adaptor)
@@ -42,11 +51,18 @@ func main() {
 		work := func() {
 			neuro.On(neuro.Event("meditation"), func(data interface{}) {
 				if currentMediation, ok := data.(uint8); ok {
-					meditation = (meditation*basePart) + (float64(currentMediation)*currentPart)
+					meditation = (meditation * basePart) + (float64(currentMediation) * currentPart)
+					if int(meditation) != 0 {
+						bar.Start()
+					}
 					bar.Set(int(meditation))
 					if meditation > border {
+						bar.Finish()
 						fmt.Println("目標達成めっちゃ集中してる！！！")
 						if err := robot.Stop(); err == nil {
+							if command != "" {
+								exec.Command(command)
+							}
 							os.Exit(1)
 						}
 					}
